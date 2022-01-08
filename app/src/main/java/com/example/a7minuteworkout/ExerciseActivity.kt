69131b4f462333b74670a928844126ac3fe14c89
Thomas Young <Thomas.young228@gmail.com>
2022-01-07 21:20:13 -0500

    added text to speech functionality

--- app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
@@ -1,14 +1,21 @@
 package com.example.a7minuteworkout
 
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.example.a7minuteworkout.databinding.ActivityExcerciseBinding
+import java.lang.Exception
+import java.util.*
+import kotlin.collections.ArrayList
 
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     // general binding syntax
     private var restTimer: CountDownTimer? = null
     private var restProgress = 0
@@ -17,6 +24,11 @@ class ExerciseActivity : AppCompatActivity() {
 
     private var exerciseList : ArrayList<ExerciseModel>? = null
     private var currentExercisePosition = -1
+
+    private var tts: TextToSpeech? = null
+
+    //media player variable
+    private var player: MediaPlayer? = null
 
 
     // create a binding variable
@@ -36,6 +48,8 @@ class ExerciseActivity : AppCompatActivity() {
         }
 
         exerciseList = Constants.defaultExerciseList()
+
+        tts = TextToSpeech(this, this)
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
@@ -47,6 +61,16 @@ class ExerciseActivity : AppCompatActivity() {
 
 
     private fun setupRestView() {
+
+        // set up media player
+        try {
+            val soundURI = Uri.parse("android.resource://com.example.a7minuteworkout/" + R.raw.press_start)
+            player = MediaPlayer.create(applicationContext, soundURI)
+            player?.isLooping = false
+            player?.start()
+        } catch (e: Exception){
+            e.printStackTrace()
+        }
 
         binding?.flRestView?.visibility = View.VISIBLE
         binding?.tvTitle?.visibility = View.VISIBLE
@@ -64,6 +88,8 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.tvUpcomingExerciseName?.text = exerciseList!![currentExercisePosition + 1].getName()
 
         setRestProgressBar()
+        speakOut("Nice Job! You can relax")
+
     }
 
     private fun setRestProgressBar() {
@@ -99,6 +125,8 @@ class ExerciseActivity : AppCompatActivity() {
             exerciseTimer?.cancel()
             exerciseProgress = 0
         }
+
+        speakOut(exerciseList!![currentExercisePosition].getName())
 
         binding?.ivImage?.setImageResource(exerciseList!![currentExercisePosition].getImage())
         binding?.tvExerciseName?.text = exerciseList!![currentExercisePosition].getName()
@@ -136,7 +164,32 @@ class ExerciseActivity : AppCompatActivity() {
             restProgress = 0
         }
         super.onDestroy()
+
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+
+        if(player != null){
+            player!!.stop()
+        }
+
         binding = null
+    }
+
+
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = tts?.setLanguage((Locale.US))
+        } else {
+            Log.e("TTS", "Initialization Failed")
+        }
+
+    }
+
+    private fun speakOut(text: String){
+        tts!!.speak(text,TextToSpeech.QUEUE_FLUSH, null, "")
     }
 }
 
